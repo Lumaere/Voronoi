3c1eda42180334ea1aa7f40f69467ac84633fd7b
Lawrence <lawrencejwu@gmail.com>
2016-12-09 19:02:27 -0500

    Moved DCEL structures into class and added memory management

--- src/DCEL/DCEL.cpp
+++ src/DCEL/DCEL.cpp
@@ -1,21 +1,145 @@
 #include "DCEL.h"
 
-std::vector<vertex*> holdV;
-std::vector<half_edge*> holdE;
+/*
+ * vertex
+ */
+DCEL::vertex::vertex(point<double> p)
+    : coordinates{p}
+{}
 
-std::map<point<double>,face*> face::hash;
+DCEL::vertex::vertex()
+    : coordinates({0,0})
+{}
 
-half_edge* half_edge_factory()
+DCEL::vertex* DCEL::vertex_factory(point<double> p)
 {
-    half_edge *ret = new half_edge;
-    ret->twin = new half_edge;
-    ret->twin->twin = ret;
+    vertex* ret = new vertex(p);
+    vertex_list.push_back(ret);
     return ret;
 }
 
-std::pair<half_edge*,half_edge*> match_face(half_edge *l, half_edge *r)
+/*
+ * face
+ */
+DCEL::face::face(point<double> p)
+    : site{p}
+{}
+
+DCEL::face::face()
+    : site({0,0})
+{}
+
+DCEL::face* DCEL::face_factory(point<double> p)
 {
-    half_edge *toP, *fromP;
+    face* ret = new face(p);
+    face_list.push_back(ret);
+    face_hash.insert(std::make_pair(p,ret));
+    return ret;
+}
+
+DCEL::face* DCEL::face_of(point<double> p)
+{
+    /* throw error if non existing */
+    return face_hash.at(p);
+}
+
+/*
+ * half_edge
+ */
+DCEL::half_edge::half_edge()
+    : prev{nullptr}, 
+      next{nullptr}, 
+      twin{nullptr},
+      tail{nullptr}, 
+      left{nullptr}
+{}
+
+void DCEL::half_edge::set_prev(half_edge *p)
+{
+    assert(this->prev == nullptr);
+    assert(p->next == nullptr);
+    this->prev = p;
+    p->next = this;
+    this->left = this->prev->left;
+}
+
+void DCEL::half_edge::set_next(half_edge *n)
+{
+    assert(this->next == nullptr);
+    assert(this->twin->twin == this);
+    this->next = n;
+    n->prev = this;
+    this->left = this->next->left;
+}
+
+DCEL::half_edge* DCEL::half_edge_factory()
+{
+    DCEL::half_edge *ret = new DCEL::half_edge;
+    ret->twin = new DCEL::half_edge;
+    ret->twin->twin = ret;
+    edge_list.push_back(ret);
+    edge_list.push_back(ret->twin);
+    return ret;
+}
+
+/*
+ * DCEL general functions
+ */
+std::tuple<std::vector<DCEL::half_edge*>,
+           std::vector<DCEL::vertex*>,
+           std::vector<DCEL::face*>>
+DCEL::to_vector() const
+{
+    return std::make_tuple(edge_list,vertex_list,face_list);
+}
+
+// checks edge properties for validity
+void DCEL::check_edges() const
+{
+#ifdef DEBUG
+    for (auto e : edge_list) {
+        if (e->prev) {
+            assert(e->prev->next == e);
+            assert(e->prev->left == e->left);
+        }
+        if (e->next) {
+            assert(e->next->prev == e);
+            assert(e->next->left == e->left);
+        }
+        assert(e->twin->twin == e);
+        if (e->tail) {
+            assert(e->twin->next->tail == e->tail);
+            assert(e->prev->twin->tail == e->tail);
+        }
+    }
+#endif
+}
+
+// add edge representatives for faces
+void DCEL::fix_faces()
+{
+    for (auto e : edge_list)
+        if (e->left->rep == nullptr)
+            e->left->rep = e;
+}
+
+DCEL::~DCEL()
+{
+    for (auto e : edge_list)
+        delete e;
+    for (auto v : vertex_list)
+        delete v;
+    for (auto f : face_list)
+        delete f;
+}
+
+/*
+ * helper functions
+ */
+std::pair<DCEL::half_edge*,DCEL::half_edge*> 
+match_face(DCEL::half_edge *l, DCEL::half_edge *r)
+{
+    DCEL::half_edge *toP, *fromP;
     if (l->left == r->left) {
         toP = l;
         fromP = r;
--- src/DCEL/DCEL.h
+++ src/DCEL/DCEL.h
@@ -4,92 +4,89 @@
 
 #include <vector>
 #include <map>
+#include <tuple>
 #include <cassert>
 #include <utility>
 
-/* does not support holes in faces */
-struct half_edge;
-struct vertex;
+class DCEL {
+public:
+    struct face;
+    struct half_edge;
+    struct vertex;
+    // create half_edge with its twin
+    // half_edge's always have a counterpart in our graphs
+    half_edge* half_edge_factory();
 
-extern std::vector<vertex*> holdV;
-extern std::vector<half_edge*> holdE;
+    // create vertex
+    vertex* vertex_factory(point<double> p);
 
-struct vertex {
-    vertex (point<double> p)
-        : coordinates{p}
-    {
-        holdV.push_back(this);
-    }
-    half_edge *rep; /* rep->tail == this */
-    point<double> coordinates;
-};
+    // create face
+    face* face_factory(point<double> p);
 
-struct face {
-    face (point<double> p)
-        : site{p}
-    {
-        assert(hash.find(p) == hash.end());
-        hash.insert(std::make_pair(p,this));
-    }
-    half_edge *rep;     /* rep->left == this */
-    point<double> site;
+    // returns face of input site -> assumes one exists
+    face* face_of(point<double> p);
 
-    static face* face_of(point<double> p)
-    {
-        /* throw error if non existing */
-        return hash.at(p);
-    }
+    // returns tuple of vectors for half_edges, vertexes, and faces
+    std::tuple<std::vector<half_edge*>,std::vector<vertex*>,std::vector<face*>>
+    to_vector() const;
+
+    // checks edge properties for validity
+    void check_edges() const;
+
+    // add edge representatives for faces
+    void fix_faces();
+
+    // custom destructor to free edges
+    ~DCEL();
+
+    struct vertex {
+        half_edge *rep; /* rep->tail == this */
+        point<double> coordinates;
+    private:
+        vertex();
+        vertex(point<double> p);
+        friend vertex* DCEL::vertex_factory(point<double> p);
+    };
+
+    /* does not support holes in faces */
+    struct face {
+        half_edge *rep;     /* rep->left == this */
+        point<double> site;
+    private:
+        face();
+        face(point<double> p);
+        friend face* DCEL::face_factory(point<double> p);
+    };
+
+    // create half_edge with factory
+    struct half_edge {
+        // sets prev edge for this - assume 'n' has geq information than this
+        void set_prev(half_edge *p);
+        // similar to set_prev
+        void set_next(half_edge *n);
+
+        half_edge *prev; /* prev->next == this */
+        half_edge *next; /* next->prev == this */
+        half_edge *twin; /* twin->twin == this */
+        vertex *tail;    /* twin->next->tail == tail &&
+                            prev->twin->tail == tail */
+        face *left;      /* prev->left == left && next->left == left */
+    private:
+        half_edge();
+        friend half_edge* DCEL::half_edge_factory();
+    };
+
 private:
-    static std::map<point<double>,face*> hash;
+    std::vector<half_edge*> edge_list;
+    std::vector<vertex*> vertex_list;
+    std::vector<face*> face_list;
+    std::map<point<double>,face*> face_hash;
 };
 
-// create half_edge with factory
-struct half_edge {
-    // sets prev edge for this - assume 'n' has geq information than this
-    void set_prev(half_edge *p)
-    {
-        assert(this->prev == nullptr);
-        assert(p->next == nullptr);
-        this->prev = p;
-        p->next = this;
-        this->left = this->prev->left;
-    }
-
-    // similar to set_prev
-    void set_next(half_edge *n)
-    {
-        assert(this->next == nullptr);
-        assert(this->twin->twin == this);
-        this->next = n;
-        n->prev = this;
-        this->left = this->next->left;
-    }
-
-    half_edge *prev; /* prev->next == this */
-    half_edge *next; /* next->prev == this */
-    half_edge *twin; /* twin->twin == this */
-    vertex *tail;    /* twin->next->tail == tail &&
-                        prev->twin->tail == tail */
-    face *left;      /* prev->left == left && next->left == left */
-private:
-    half_edge()
-        : prev{nullptr}, 
-          next{nullptr}, 
-          twin{nullptr},
-          tail{nullptr}, 
-          left{nullptr}
-    {
-        holdE.push_back(this);
-    }
-    friend half_edge* half_edge_factory();
-};
-
-// create half_edge with its twin
-// half_edge's always have a counterpart in our graphs
-half_edge* half_edge_factory();
 
 // input: two half_edges with either themselves or twins pointing to a common
 //        face
 // returns: pair of half_edges that point to common face
-std::pair<half_edge*,half_edge*> match_face(half_edge *l, half_edge *r);
+std::pair<DCEL::half_edge*,DCEL::half_edge*> 
+match_face(DCEL::half_edge *l, DCEL::half_edge *r);
 
